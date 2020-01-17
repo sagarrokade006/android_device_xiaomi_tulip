ba632b72dbf1e84c44d1789488e3798bed731785
akhilmsachu <akhilsree001@gmail.com>
2020-01-17 22:46:37 +0530

    havoc bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_twolip.mk
+    $(LOCAL_DIR)/havoc_twolip.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_twolip-user \
-    lineage_twolip-userdebug \
-    lineage_twolip-eng
+    havoc_twolip-user \
+    havoc_twolip-userdebug \
+    havoc_twolip-eng
