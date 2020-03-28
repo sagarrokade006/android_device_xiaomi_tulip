f82a0f9adf7e6eb527ea240b271eecf983344d66
Pratik-sys <pithore.pratik@gmail.com>
2020-03-28 15:00:38 +0530

    tulip: update FP to 11.0.3.0 PEKMIXM
    Signed-off-by: Pratik-sys <pithore.pratik@gmail.com>

--- havoc_twolip.mk
+++ havoc_twolip.mk
@@ -36,7 +36,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := tulip
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tulip-user 9 PKQ1.180904.001 V10.3.2.0.PEKMIXM release-keys"
+    PRIVATE_BUILD_DESC="tulip-user 9 PKQ1.180904.001 V11.0.3.0.PEKMIXM release-keys"
 
 BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys"
 
